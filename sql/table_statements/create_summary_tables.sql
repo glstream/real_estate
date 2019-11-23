cb9ce4f4565bfea863edce9436e40f37da194784
glstream <grayson.stream@gmail.com>
2019-11-23 14:54:29 -0800

    new users

---
+++ sql/table_statements/create_summary_tables.sql
@@ -0,0 +1,68 @@
+CREATE TABLE IF NOT EXISTS dim_regions (
+	region_name VARCHAR(100) NOT NULL,
+	region_id VARCHAR(40) NOT NULL,
+    
+    PRIMARY KEY ( region_id )
+    );
+    
+    
+    
+CREATE TABLE IF NOT EXISTS res_home_data_summary (
+	month_year VARCHAR(40) NULL,
+	region_id  VARCHAR(40) NOT NULL,
+    new_listing_curr_year  VARCHAR(40) NULL,
+    new_listing_prev_year  VARCHAR(40) NULL,
+    total_active_curr_year  VARCHAR(40) NULL,
+    total_active_prev_year  VARCHAR(40) NULL,
+    percent_chg_total_active  VARCHAR(40) NULL,
+    pending_curr_year VARCHAR(40) NULL,
+    pending_prev_year VARCHAR(40) NULL,
+    percent_chg_total_pending VARCHAR(40) NULL,
+    closed_curr_year VARCHAR(40) NULL,
+    closed_prev_year VARCHAR(40) NULL,
+	percent_chg_total_closed VARCHAR(40) NULL,
+    median_price_curr_year VARCHAR(40) NULL,
+    median_price_prev_year VARCHAR(40) NULL,
+    precent_chg_median_price VARCHAR(40) NULL,
+    months_of_inventory VARCHAR(40) NULL
+    );
+CREATE TABLE IF NOT EXISTS res_condo_home_data_summary (
+	month_year VARCHAR(40) NULL,
+	region_id  VARCHAR(40) NOT NULL,
+    new_listing_curr_year  VARCHAR(40) NULL,
+    new_listing_prev_year  VARCHAR(40) NULL,
+    total_active_curr_year  VARCHAR(40) NULL,
+    total_active_prev_year  VARCHAR(40) NULL,
+    percent_chg_total_active  VARCHAR(40) NULL,
+    pending_curr_year VARCHAR(40) NULL,
+    pending_prev_year VARCHAR(40) NULL,
+    percent_chg_total_pending VARCHAR(40) NULL,
+    closed_curr_year VARCHAR(40) NULL,
+    closed_prev_year VARCHAR(40) NULL,
+	percent_chg_total_closed VARCHAR(40) NULL,
+    median_price_curr_year VARCHAR(40) NULL,
+    median_price_prev_year VARCHAR(40) NULL,
+    precent_chg_median_price VARCHAR(40) NULL,
+    months_of_inventory VARCHAR(40) NULL
+    );
+CREATE TABLE IF NOT EXISTS condo_home_data_summary (
+	month_year VARCHAR(40) NULL,
+	region_id  VARCHAR(40) NOT NULL,
+    new_listing_curr_year  VARCHAR(40) NULL,
+    new_listing_prev_year  VARCHAR(40) NULL,
+    total_active_curr_year  VARCHAR(40) NULL,
+    total_active_prev_year  VARCHAR(40) NULL,
+    percent_chg_total_active  VARCHAR(40) NULL,
+    pending_curr_year VARCHAR(40) NULL,
+    pending_prev_year VARCHAR(40) NULL,
+    percent_chg_total_pending VARCHAR(40) NULL,
+    closed_curr_year VARCHAR(40) NULL,
+    closed_prev_year VARCHAR(40) NULL,
+	percent_chg_total_closed VARCHAR(40) NULL,
+    median_price_curr_year VARCHAR(40) NULL,
+    median_price_prev_year VARCHAR(40) NULL,
+    precent_chg_median_price VARCHAR(40) NULL,
+    months_of_inventory VARCHAR(40) NULL
+    );
+    
+    
