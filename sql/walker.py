add3bf51fe7614d1735beac6f47de3523bbc89bf
Grayson Stream <grayson.stream@gmail.com>
2019-11-23 22:03:30 -0800

    added load month to path

--- sql/walker.py
+++ sql/walker.py
@@ -2,13 +2,15 @@ import os
 import sys
 cwd = os.getcwd()
 print(cwd)
-newdir = os.chdir('.././data/PUB/')
+newdir = '/home/pi/Documents/project-file/real_estate/pdfs/'
 
 print(newdir)
 # print(pub_real_dir)
 
 
-# for (dirs, subDir, files) in os.walk(newdir):
-#     print(files)
+for (dirs, subDir, files) in os.walk(newdir):
+    print(files)
 
 print(sys.platform)
+
+/home/pi/Documents/project-file/real_estate/pdfs/KCBreakouts_Oct.pdf
