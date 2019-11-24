3a1902239706113760604977704a1a1e7749e926
glstream <grayson.stream@gmail.com>
2019-11-23 23:36:01 -0800

    added region inserts

---
+++ sql/inserts/regions.sql
@@ -0,0 +1,9 @@
+INSERT INTO dim_regions (region_name, region_id)
+	VALUES
+    ("W Seattle", "140"),
+    ("N Seattle", "710"),
+    ("Central Seattle SE, Leshi, Mt Baker", "380"),
+    ("Central Seattle SW, Beacon Hill", "385"),
+    ("Queen Anne", "700"),
+    ("Central Seattle, Capitol Hill", "390"),
+    ("Ballard, Greenlake", "705")
